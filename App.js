bb3d3c431dbc5e9d03e993e913c7c3022bd7f983
Siddharth Jain <jsidd06@gmail.com>
2023-10-30 06:55:17 +0530

    feat: change ui and test user input test in console.log

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [enterGoal, setEnterGoal] = useState("");
+  function addGoalsHandler(text) {
+    setEnterGoal(text);
+  }
+  function submitGoalsHandler() {
+    console.log(enterGoal);
+  }
   return (
     <View style={styles.container}>
-      <View>
-        <TextInput placeholder="Enter your goals course" />
-        <Button title="Add Goal" />
+      <View style={styles.textInputContainer}>
+        <TextInput
+          style={styles.input}
+          onChangeText={addGoalsHandler}
+          placeholder="Enter your goals course"
+        />
+        <Button title="Add Goal" onPress={submitGoalsHandler} />
       </View>
-      <View>
+      <View style={styles.textContainer}>
         <Text>Lets add goals..</Text>
       </View>
     </View>
@@ -16,6 +28,26 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     padding: 50,
+    paddingHorizontal: 20,
+  },
+  textInputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  input: {
+    width: "75%",
+    borderWidth: 1,
+    borderRadius: 2,
+    padding: 5,
+  },
+  textContainer: {
+    flex: 5,
+    marginTop: 20,
   },
 });
