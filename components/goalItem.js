610886183e1a63811f6ee599a5b5a860a38d287e
Siddharth Jain <jsidd06@gmail.com>
2023-10-31 07:23:04 +0530

    feat: pressed item style using on ios

--- components/goalItem.js
+++ components/goalItem.js
@@ -1,14 +1,17 @@
-import { Pressable, StyleSheet, Text } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const GoalItems = (props) => {
   return (
-    <Pressable
-      onPress={props.onDeleteItem.bind(this, props.id)}
-      style={styles.goalContainer}
-    >
-      <Text style={styles.goalText}>{props.text}</Text>
-    </Pressable>
+    <View style={styles.goalContainer}>
+      <Pressable
+        android_ripple={{ color: "#7743DB" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
+    </View>
   );
 };
 
@@ -16,15 +19,18 @@ export default GoalItems;
 
 const styles = StyleSheet.create({
   goalContainer: {
-    padding: 5,
     borderWidth: 1,
     borderColor: "#7743DB",
     margin: 5,
     borderRadius: 5,
   },
+  pressedItem: {
+    opacity: 0.6,
+  },
   goalText: {
     color: "#000",
     fontSize: 16,
     fontWeight: "500",
+    padding: 5,
   },
 });
